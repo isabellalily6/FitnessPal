ad9d24bb85e51ee02c1a6efc6930d1daf4cbdfd8
Isabella Tomaz-Ketley <tomazisab@myvuw.ac.nz>
2021-10-18 18:14:19 +1300

    Added a map to the app
    Added a map using the google API

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import { Geolocation } from '@capacitor/geolocation';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+
+declare var google: any;
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +14,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  locations: Observable<any>;
+  locationsCollection: AngularFirestoreCollection<any>;
+
+    // Map related
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+    markers = [];
+   
+    // Misc
+    isTracking = false;
+    watch: string;
+    user = null;
 
   constructor() {}
+
+  ionViewDidEnter(){
+    this.showMap();
+  }
+
+  showMap() {
+    let latLng = new google.maps.LatLng(51.9036442, 7.6673267);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 
 }
